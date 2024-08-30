cd0fe63117ace4e1ab8e8abe3d9b54bd5a3de0ec
Riddhi Soni <riddhisoni2020@gmail.com>
2024-08-31 00:01:58 +0530

    project layout completed

---
+++ script.js
@@ -0,0 +1,20 @@
+let string = "";
+let inputs = document.getElementById('input');
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+      if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'C'){
+        string = " "
+        document.querySelector('input').value = string;
+      }
+      else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+      }
+    })
+})
